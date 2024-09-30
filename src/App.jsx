8881d01a86091f5c447eb025faac499a193a56b2
iTanvirSingh <71079808+tanviirsinghh@users.noreply.github.com>
2024-09-30 21:17:23 +0530

    responsive message

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,51 @@ import ZigZagSection from "./Components/ZigZagSection"
 // import FifthSection from "./Components/FifthSection"
 import Footer from "./Components/Footer"
 import LastSection from "./Components/LastSection"
-function App() {
+import { useState, useEffect } from "react";  // Added useEffect import here
 
+function App() {
+  const [width, setWidth] = useState(false);
+
+  useEffect(() => {
+    const handleWidth = () => {
+      setWidth(window.innerWidth < 1000);
+    };
+
+    window.addEventListener('resize', handleWidth);
+
+    // Call handleWidth initially to set the initial state
+    handleWidth();
+
+    return () => {
+      window.removeEventListener('resize', handleWidth);
+    };
+  }, []);
 
   return (
-    <>
-   <MainSection/>
-   <SecondSection/>
-   <ThirdSection/>
-   <ZigZagSection/>
-   <FourthSection/>
-  
-   {/* <FifthSection/> */}
-  < LastSection/>
-  <Footer/>
- 
-    </>
-  )
+    <div className="">
+      {!width && (
+        <div>
+          <MainSection />
+          <SecondSection />
+          <ThirdSection />
+          <ZigZagSection />
+          <FourthSection />
+          <LastSection />
+          <Footer />
+        </div>
+      )}
+
+      {width && (
+        <div className="bg-green-950  h-screen w-full flex flex-col justify-center items-center">
+          <div><img className="w-24" src="../img/NobleStitch-removebg-preview.png" alt="" />
+          </div>
+        <div className=" p-9 text-center font-bold h-18 w-full bg-green-950 ">
+          <p className="text-peela font-custom">Please open link in Laptop or PC for better experience</p>
+        </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
