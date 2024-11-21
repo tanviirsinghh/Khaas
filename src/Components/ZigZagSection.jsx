8ba40ed7ee2713617ce4c8fa8f9a0fa272b75c61
iTanvirSingh <71079808+tanviirsinghh@users.noreply.github.com>
2024-11-21 11:07:17 +0530

    changed

--- src/Components/ZigZagSection.jsx
+++ src/Components/ZigZagSection.jsx
@@ -91,7 +91,7 @@ export default function ZigZagSection() {
             <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
               {/* Image */}
               <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1" data-aos="fade-up">
-                <img className="max-w-full mx-auto md:max-w-none h-auto" src="public/Khass/460367170_1565894984013692_562212362003629983_n.jpg" width={540} height={405} alt="Features 03" />
+                <img className="max-w-full mx-auto md:max-w-none h-auto" src="../Khass/460367170_1565894984013692_562212362003629983_n.jpg" width={540} height={405} alt="Features 03" />
               </div>
               {/* Content */}
               <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-right">
